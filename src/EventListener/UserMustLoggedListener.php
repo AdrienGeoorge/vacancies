ec5b17c3927245cf1d2609921861acd6ce46e154
AdrienGeoorge <adrien-george@outlook.fr>
2024-10-01 13:34:12 +0200

    Ajout d'un listener si l'utilisateur n'est pas connecté pour le rediriger vers la page de connexion

---
+++ src/EventListener/UserMustLoggedListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserMustLoggedListener implements EventSubscriberInterface
+{
+    private TokenStorageInterface $tokenStorage;
+    private RouterInterface $router;
+
+    public function __construct(TokenStorageInterface $tokenStorage, RouterInterface $router)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->router = $router;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$this->tokenStorage->getToken()) {
+            if ('/login' !== $event->getRequest()->getPathInfo() && '/register' !== $event->getRequest()->getPathInfo()) {
+                $response = new RedirectResponse($this->router->generate('auth_login'));
+                $event->setResponse($response);
+            }
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
+    }
+}

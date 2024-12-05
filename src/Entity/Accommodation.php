4d56b2ad74a607583cc77c8a362d3f2019831754
AdrienGeoorge <adrien-george@outlook.fr>
2024-12-06 00:15:00 +0100

    Ajout d'un hôtel au voyage

---
+++ src/Entity/Accommodation.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccommodationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AccommodationRepository::class)]
+class Accommodation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 500)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $zipCode = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $country = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $additionalExpenses = null;
+
+    #[ORM\Column]
+    private bool $booked = false;
+
+    #[ORM\ManyToOne(inversedBy: 'accomodations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Trip $trip = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(?string $zipCode): static
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getAdditionalExpenses(): ?float
+    {
+        return $this->additionalExpenses;
+    }
+
+    public function setAdditionalExpenses(?float $additionalExpenses): static
+    {
+        $this->additionalExpenses = $additionalExpenses;
+
+        return $this;
+    }
+
+    public function isBooked(): bool
+    {
+        return $this->booked;
+    }
+
+    public function setBooked(bool $booked): static
+    {
+        $this->booked = $booked;
+
+        return $this;
+    }
+
+    public function getTrip(): ?Trip
+    {
+        return $this->trip;
+    }
+
+    public function setTrip(?Trip $trip): static
+    {
+        $this->trip = $trip;
+
+        return $this;
+    }
+}

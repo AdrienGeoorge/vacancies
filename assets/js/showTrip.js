219f035691507640ec942aacf68172492d18894b
AdrienGeoorge <adrien-george@outlook.fr>
2024-12-13 16:01:22 +0100

    Ajout d'une dépense supplémentaire au voyage

--- assets/js/showTrip.js
+++ assets/js/showTrip.js
@@ -5,51 +5,54 @@ import Routing from "fos-router"
 
 const tripId = document.getElementById('tripId')
 const ctx = document.getElementById('budgetChart')
-const budgetNone = document.getElementById('budgetNone')
 
-axios({
-    method: 'get',
-    url: Routing.generate('trip_get_budget', {'id': tripId.value}),
-    headers: {'X-Requested-With': 'XMLHttpRequest'}
-})
-    .then(response => {
-        if (response.data.paid > 0 || response.data.toPay > 0) {
-            new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    labels: ['Payé', 'A payer'],
-                    datasets: [{
-                        label: ' Montant',
-                        data: [response.data.paid, response.data.toPay],
-                        backgroundColor: [
-                            'rgb(20 184 166)',
-                            'rgb(239 68 68)',
-                        ],
-                        borderWidth: 0
-                    }]
-                },
-                options: {
-                    tooltips: {
-                        enabled: false
+if (tripId && ctx) {
+    const budgetNone = document.getElementById('budgetNone')
+
+    axios({
+        method: 'get',
+        url: Routing.generate('trip_get_budget', {'id': tripId.value}),
+        headers: {'X-Requested-With': 'XMLHttpRequest'}
+    })
+        .then(response => {
+            if (response.data.paid > 0 || response.data.toPay > 0) {
+                new Chart(ctx, {
+                    type: 'doughnut',
+                    data: {
+                        labels: ['Payé', 'A payer'],
+                        datasets: [{
+                            label: ' Montant',
+                            data: [response.data.paid, response.data.toPay],
+                            backgroundColor: [
+                                'rgb(20 184 166)',
+                                'rgb(239 68 68)',
+                            ],
+                            borderWidth: 0
+                        }]
                     },
-                    plugins: {
-                        legend: {
-                            display: false,
+                    options: {
+                        tooltips: {
+                            enabled: false
                         },
-                        datalabels: {
-                            formatter: (value) => {
-                                return value + ' €'
+                        plugins: {
+                            legend: {
+                                display: false,
                             },
-                            color: '#fff',
-                        }
+                            datalabels: {
+                                formatter: (value) => {
+                                    return value + '€'
+                                },
+                                color: '#fff',
+                            }
+                        },
                     },
-                },
-                plugins: [ChartDataLabels],
-            })
-        } else {
-            ctx.classList.add('hidden')
-            budgetNone.innerHTML = '<div class="text-center text-sm">Le graphique ne peut pas charger pour le moment car aucune dépense n\'a été saisie pour ce voyage.</div>'
-        }
-    })
-    .catch(() => {
-    })
+                    plugins: [ChartDataLabels],
+                })
+            } else {
+                ctx.classList.add('hidden')
+                budgetNone.innerHTML = '<div class="text-center text-sm">Le graphique ne peut pas charger pour le moment car aucune dépense n\'a été saisie pour ce voyage.</div>'
+            }
+        })
+        .catch(() => {
+        })
+}
---
+++ src/Form/VariousExpensiveType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\VariousExpensive;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VariousExpensiveType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'w-full px-8 py-4 rounded-2xl font-medium bg-gray-100 border border-gray-200
+                              placeholder-gray-400 text-sm
+                              focus:outline-none focus:border-gray-400 focus:bg-white
+                              dark:bg-transparent dark:border-gray-300 dark:focus:bg-transparent
+                              dark:placeholder-gray-300',
+                    'placeholder' => 'Nom de la dépense',
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'w-full px-8 py-4 rounded-2xl font-medium bg-gray-100 border border-gray-200
+                              placeholder-gray-400 text-sm
+                              focus:outline-none focus:border-gray-400 focus:bg-white
+                              dark:bg-transparent dark:border-gray-300 dark:focus:bg-transparent
+                              dark:placeholder-gray-300',
+                    'placeholder' => 'Quelles sont les modalités à connaître ? Où se situe cette activité ?',
+                ]
+            ])
+            ->add('price', NumberType::class, [
+                'attr' => [
+                    'class' => 'w-full px-8 py-4 rounded-2xl font-medium bg-gray-100 border border-gray-200
+                              placeholder-gray-400 text-sm
+                              focus:outline-none focus:border-gray-400 focus:bg-white
+                              dark:bg-transparent dark:border-gray-300 dark:focus:bg-transparent
+                              dark:placeholder-gray-300',
+                    'placeholder' => 'Montant de la dépense',
+                ]
+            ])
+            ->add('perPerson', CheckboxType::class, [
+                'required' => false,
+                'attr' => [
+                    'style' => 'width: 1.5em; height: 1.5em;'
+                ]
+            ])
+            ->add('paid', CheckboxType::class, [
+                'required' => false,
+                'attr' => [
+                    'style' => 'width: 1.5em; height: 1.5em;'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => VariousExpensive::class,
+        ]);
+    }
+}
